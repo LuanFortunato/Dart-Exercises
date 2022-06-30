0f50ef639c15a4cfdebb701c2d0f9dae11a9edde
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-06-30 16:26:48 -0300

    [change] identation

--- desafio1.dart
+++ desafio1.dart
@@ -1,9 +1,13 @@
 import 'dart:math';
 
 void main() {
+  print('Função 1');
   desafio1();
+  print('Função 2');
+  desafio2();
 }
 
+//Cria um vetor de números aleatórios e ordena
 void desafio1() {
   List<int> numeros = criaVetorNumeros();
 
@@ -12,11 +16,12 @@ void desafio1() {
   listaVetor(numeros);
 }
 
+//Soma todos os números que não terminam em zero, que, se somados com seu inverso, dão um resultado ímpar e menor que um milhão
 void desafio2() {
   List numeros = [];
   int total = 0;
 
-  for (var i = 0; i < 1000000; i++) {
+  for (int i = 0; i < 1000000; i++) {
     numeros = i.toString().split('');
 
     List inverso = numeros.reversed.toList();
@@ -37,7 +42,7 @@ void desafio2() {
 List<int> criaVetorNumeros() {
   List<int> numeros = [];
 
-  for (var i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     Random numero = Random();
     numeros.add(numero.nextInt(100));
   }
@@ -46,14 +51,14 @@ List<int> criaVetorNumeros() {
 }
 
 void listaVetor(List<int> lista) {
-  for (var i = 0; i < lista.length; i++) {
+  for (int i = 0; i < lista.length; i++) {
     print(lista[i]);
   }
 }
 
 List<int> sort(List<int> lista) {
-  for (var i = 0; i < lista.length; i++) {
-    for (var j = 0; j < lista.length - 1 - i; j++) {
+  for (int i = 0; i < lista.length; i++) {
+    for (int j = 0; j < lista.length - 1 - i; j++) {
       if (lista[j] > lista[j + 1]) {
         int aux = lista[j];
         lista[j] = lista[j + 1];
