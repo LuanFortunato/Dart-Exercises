1183b899c89c05eadfedfb4b564227251160ba49
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-07-01 10:38:49 -0300

    [feat] more operators

--- opreradores.dart
+++ opreradores.dart
@@ -18,4 +18,25 @@ void main() {
   // print(nota1 <= nota2);
   // print(nota1 == nota2);
   // print(nota1 != nota2);
+
+  // Logicos
+  // nota1 = 25;
+  // nota1 += 30;
+  // nota1 -= 20;
+  // nota1 *= 3;
+  // nota1 /= 4;
+  // nota1 ~/= 4;
+  // print(nota1);
+
+  //Type test
+  // double numero = 10;
+  // String numero2 = '10';
+  // int numero3 = 10;
+  // print(numero is double);
+  // print(numero is! int);
+  // print(numero2 is double);
+  // print(numero2 is! String);
+  // print(numero3 is double);
+  // print(numero3 is! String);
+
 }
