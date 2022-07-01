37016ed0b90fd6c318e5d5a283594aa51fe65f1b
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-07-01 09:27:04 -0300

    [feat] operators

---
+++ opreradores.dart
@@ -0,0 +1,21 @@
+void main() {
+  double nota1 = 10;
+  double nota2 = 10;
+
+  //Aritmeticos
+  // print(nota1 + nota2);
+  // print(nota1 - nota2);
+  // print(-nota1);
+  // print(nota1 * nota2);
+  // print(nota1 / nota2);
+  // print(nota1 ~/ nota2);
+  // print(nota1 % nota2);
+
+  //Logicos
+  // print(nota1 > nota2);
+  // print(nota1 >= nota2);
+  // print(nota1 < nota2);
+  // print(nota1 <= nota2);
+  // print(nota1 == nota2);
+  // print(nota1 != nota2);
+}
